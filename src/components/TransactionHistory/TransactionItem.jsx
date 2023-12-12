680c995106143ba8aa52eebef138ac96b3f05956
GermanSTr <gershev1994@gmail.com>
2023-12-12 16:01:41 +0200

    task-04

--- src/components/TransactionHistory/TransactionItem.jsx
+++ src/components/TransactionHistory/TransactionItem.jsx
@@ -3,7 +3,7 @@ import css from './TransactionHistory.module.css';
 const TransactionItem = ({ type, amount, currency }) => {
   return (
     <tbody>
-      <tr>
+      <tr className={css.line}>
         <td>{type}</td>
         <td>{amount}</td>
         <td>{currency}</td>
