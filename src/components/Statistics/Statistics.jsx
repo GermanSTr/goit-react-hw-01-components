42a3695371499e59eed300d12446ea1e9634fbf1
GermanSTr <gershev1994@gmail.com>
2023-12-12 16:52:14 +0200

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import { StatsList } from './StatsList';
 const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title.length > 0 && <h2 className={css.title}>{title}</h2>}
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.stat}>
         {stats?.map(stat => {
           return (
